463dd318aa23ea1ce69874f876cdd8f89199c147
ssunaa <seonflower91@gmail.com>
2020-05-29 23:57:59 +0900

    Spring Data Jpa 적용

---
+++ src/main/java/com/may/seonhwa/domain/posts/Posts.java
@@ -0,0 +1,30 @@
+package com.may.seonhwa.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
