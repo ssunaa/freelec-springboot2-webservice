9a7f0a7cc6fc48cf511057c3bf8bade973cf79a0
ssunaa <seonflower91@gmail.com>
2020-06-01 23:03:23 +0900

    API 수정기능 추가

--- src/main/java/com/may/seonhwa/service/PostsService.java
+++ src/main/java/com/may/seonhwa/service/PostsService.java
@@ -1,7 +1,10 @@
 package com.may.seonhwa.service;
 
+import com.may.seonhwa.domain.posts.Posts;
 import com.may.seonhwa.domain.posts.PostsRepository;
+import com.may.seonhwa.web.dto.PostsResponseDto;
 import com.may.seonhwa.web.dto.PostsSaveRequestDto;
+import com.may.seonhwa.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +21,23 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당게시물이 없습니다. id=" + id));
+     
+        
+        //update시에 Repository를 호출하는부분이 없는이유는 JPA의 영속성 컨텍스트 때문
+        //TODO 더티체킹
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당게시물이 없습니다. id=" + id));
+
+        return new PostsResponseDto(posts);
+    }
+
 }
--- src/main/java/com/may/seonhwa/web/PostsApiController.java
+++ src/main/java/com/may/seonhwa/web/PostsApiController.java
@@ -1,11 +1,10 @@
 package com.may.seonhwa.web;
 
 import com.may.seonhwa.service.PostsService;
+import com.may.seonhwa.web.dto.PostsResponseDto;
 import com.may.seonhwa.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -13,9 +12,24 @@ public class PostsApiController {
 
     private final PostsService postsService; //final 안해주면 오류나는데 원인 모름 ㅠ
 
+    /**
+     * 등록or수정
+     * @param requestDto
+     * @return
+     */
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
+    }
+
+    /**
+     * ID찾기
+     * @param id
+     * @return
+     */
+    @GetMapping("/api/v1/posts")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
     }
 
 }
