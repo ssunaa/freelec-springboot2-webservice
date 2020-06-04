b2c7bf2372601060987910b9213f2f1be9c4f658
ssunaa <seonflower91@gmail.com>
2020-06-04 16:44:26 +0900

    index 페이지 생성(테스크코드 작성)

---
+++ src/main/java/com/may/seonhwa/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package com.may.seonhwa.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass //Entity클래스에서 createdDate, modifiedDate필드를 컬럼으로 인식하게 설정
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+
+}
