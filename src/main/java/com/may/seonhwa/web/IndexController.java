5fbcac1a7b45a10f7710ed227cf82700021dd0ef
ssunaa <seonflower91@gmail.com>
2020-06-05 12:16:49 +0900

    게시글 등록 완료(머스테치 화면추가)

--- src/main/java/com/may/seonhwa/web/IndexController.java
+++ src/main/java/com/may/seonhwa/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.may.seonhwa.web;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    //메인페이지 호출
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    //등록페이지 호출
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
     }
 }
