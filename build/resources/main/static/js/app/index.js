954b70ed06e9c40703d4868cc44592b8d083bc0e
ssunaa <seonflower91@gmail.com>
2020-06-06 01:53:54 +0900

    게시글 수정 완료

--- build/resources/main/static/js/app/index.js
+++ build/resources/main/static/js/app/index.js
@@ -4,6 +4,9 @@ var index = {
         $("#btn-save").on("click", function() {
             _this.save();
         });
+        $("#btn-update").on("click", function() {
+            _this.update();
+        });
     },
     save : function() {
         var data = {
@@ -23,6 +26,26 @@ var index = {
             window.location.href='/';
         }).fail(function(error) {
             alert('등록중 오류가 발생했습니다.\n 관리자에게 문의하세요.');
+            console.log(JSON.stringify(error));
+        });
+    },
+    update : function() {
+        var data = {
+            title : $("#title").val()
+          , content : $("#content").val()
+        };
+
+        $.ajax({
+            type : 'PUT'
+          , url : '/api/v1/posts/' + $("#id").val()
+          , dataType : 'json'
+          , contentType : 'application/json; charset=utf-8'
+          , data : JSON.stringify(data)
+        }).done(function() {
+            alert('글이 수정 되었습니다.');
+            window.location.href='/';
+        }).fail(function(error) {
+            alert('수정중 오류가 발생했습니다.\n 관리자에게 문의하세요.');
             console.log(JSON.stringify(error));
         });
     }
