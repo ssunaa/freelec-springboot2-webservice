b43523403792a5768bbddf11449261cce2f4ce30
ssunaa <seonflower91@gmail.com>
2020-06-08 01:50:41 +0900

    OAuth2.0 구글로그인 추가

--- src/main/java/com/may/seonhwa/web/IndexController.java
+++ src/main/java/com/may/seonhwa/web/IndexController.java
@@ -1,18 +1,21 @@
 package com.may.seonhwa.web;
 
+import com.may.seonhwa.config.auth.dto.SessionUser;
 import com.may.seonhwa.service.PostsService;
-import com.may.seonhwa.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService; //final 안해주면 오류나는데 원인 모름 ㅠ
+    private final HttpSession httpSession;
 
 
     /**
@@ -23,6 +26,10 @@ public class IndexController {
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+        if (user!=null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
@@ -44,4 +51,5 @@ public class IndexController {
         model.addAttribute("posts", postsService.findById(id));
         return "posts-update";
     }
+
 }
